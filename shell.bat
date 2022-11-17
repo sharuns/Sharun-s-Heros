32691c1fe24e7725695a56a50daf3ba9d87ec5e0
sharun <sharuns93@gmail.com>
2022-11-17 22:28:28 +0530

    Log : Moved the code to E: Drive hence editing the shell file
    Signed-off-by: sharun <sharuns93@gmail.com>

--- shell.bat
+++ shell.bat
@@ -2,7 +2,7 @@
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 
-echo set path = C:\Dev\CPP\MollyRocket_Handmade_hero_series\Sharun-s-Heros;%PATH%
+echo set path = E:\Dev\CPP\MollyRocket_Handmade_hero_series\Sharun-s-Heros;%PATH%
 
 cd "E:\Dev\CPP\MollyRocket_Handmade_hero_series\Sharun-s-Heros\code"
 
